50e31f3ff9a06007949174f1874153f46c43d3c7
Danylo Anatoliev <amongstreality@gmail.com>
2023-02-02 16:47:06 +0200

    Added first game level
    Added code to visualize game level

--- PackMan.py
+++ PackMan.py
@@ -11,3 +11,61 @@ screen = pygame.display.set_mode([width, height])  # making screen by height and
 timer = pygame.time.Clock()                        # creating to help track time
 fps = 60                                           # frames per second
 font = pygame.font.SysFont('calibri', 20)          # setting font
+
+level = lvl_1
+color_1 = '#0094ff' # color of walls on a map
+color_2 = 'white'   # color of small dots
+color_3 = '#fff5cc' # color of big dots
+
+def draw_board():   # function to draw map
+    height_tile = ((height - 50) // 32) # height specialy for tile ( /32 because in original pacman there are 32 different vertical tiles)
+    width_tile = (width // 30)          # width specialy for tile ( /30 because in original pacman there are 30 different horizontal tiles)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            # drawing dots
+            if level[i][j] == 1:   # if element in board equals 1 then as we already described in file for us we draw small circle
+                pygame.draw.circle(screen, color_2, (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), 5)
+
+            if level[i][j] == 2:   # if element in board equals 2 then as we already described in file for us we draw big circle
+                pygame.draw.circle(screen, color_3, (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), 10)
+
+            # drawing lines
+            if level[i][j] == 3:   # if element in board equals 3 then as we already described in file for us we draw vertical boarder
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile), (j * width_tile + (0.5 * width_tile), i * height_tile + height_tile), 3) # vertical line
+
+            if level[i][j] == 4:   # if element in board equals 4 then as we already described in file for us we draw horizontal boarder
+                pygame.draw.line(screen, color_1, (j * width_tile, i * height_tile + (0.5 * height_tile)), (j * width_tile + width_tile, i * height_tile + (0.5 * height_tile)), 3) # horizontal line
+
+            # drawing corners
+            if level[i][j] == 5:   # if element in board equals 5 then as we already described in file for us we draw corner from left to down
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), (j * width_tile + (0.5 * width_tile), i * height_tile + height_tile), 3) # vertical part
+                pygame.draw.line(screen, color_1, (j * width_tile, i * height_tile + (0.5 * height_tile)), (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), 3) # horizontal part
+
+            if level[i][j] == 6:   # if element in board equals 6 then as we already described in file for us we draw corner from down to right
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), (j * width_tile + (0.5 * width_tile), i * height_tile + height_tile), 3) # vertical part
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), (j * width_tile + width_tile, i * height_tile + (0.5 * height_tile)), 3) # horizontal line
+
+            if level[i][j] == 7:   # if element in board equals 7 then as we already described in file for us we draw corner from up to right
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile), (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), 3) # vertical part
+                pygame.draw.line(screen, color_1, (j * width_tile + (width_tile * 0.5), i * height_tile + (0.5 * height_tile)), (j * width_tile + width_tile, i * height_tile + (0.5 * height_tile)), 3) # horizontal part
+
+            if level[i][j] == 8:   # if element in board equals 8 then as we already described in file for us we draw corner from left to up
+                pygame.draw.line(screen, color_1, (j * width_tile + (0.5 * width_tile), i * height_tile), (j * width_tile + (0.5 * width_tile), i * height_tile + (0.5 * height_tile)), 3) # vertical part
+                pygame.draw.line(screen, color_1, (j * width_tile, i * height_tile + (0.5 * height_tile)), (j * width_tile + (width_tile * 0.5), i * height_tile + (0.5 * height_tile)), 3) # horizontal part
+
+            # drawing gates
+            if level[i][j] == 9:   # if element in board equals 9 then as we already described in file for us we draw gates for ghosts
+                pygame.draw.line(screen, color_2, (j * width_tile, i * height_tile + (0.5 * height_tile)), (j * width_tile + width_tile, i * height_tile + (0.5 * height_tile)), 3)
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+    draw_board()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
