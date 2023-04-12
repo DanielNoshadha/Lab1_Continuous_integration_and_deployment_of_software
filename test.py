15c41f3bb58adaebe4ac89222c08c37801845a83
Dan <amongstreality@gmail.com>
2023-04-12 16:56:01 +0300

    Fix

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from PackMan import move_packman
 
+
 def test_move_packman():
     packman_x = 100
     packman_y = 100
-
 
     # Test moving Pacman to the right
     new_x, new_y = move_packman(packman_x, packman_y)
