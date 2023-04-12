77c41ecfeb27b33146293b06eca150facaaa1164
Dan <amongstreality@gmail.com>
2023-04-12 17:24:59 +0300

    Revert "new test"
    This reverts commit 74f6f333b52f1bd400026ff1d36db727c088c1db.

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 import pytest
-from PackMan import draw_stuff, move_packman, direction, turns_allowed
+from PackMan import draw_stuff
 
 
 def test_draw_stuff():
     with pytest.raises(TypeError):
         draw_stuff(10, 20)
-
-
-def test_move_packman():
-    with pytest.raises(TypeError):
-        direction = 0
-        turns_allowed[0]
-        move_packman(450, 663)
